e474a7df85c906a174345fe1d9b586c812a2657c
roxfr <roxfr@outlook.fr>
2024-04-11 09:29:45 +0200

    Création de la partie front de l'application

--- app.py
+++ app.py
@@ -1,34 +1,47 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import joblib
 
 
 app = Flask(__name__)
 
-# Chargement du modèle pré-entraîné
-model = joblib.load('data/modele_random_forest.pkl')
-
 # Chargement des données
 data = pd.read_csv('data/5000_movies_cleaned.csv')
 
-# Définition de la route pour la prédiction
-# URL pour test => http://localhost:5000/predict?title=avatar
+# Chargement du modèle pré-entraîné
+model = None
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/predict', methods=['GET'])
-def predict_imdb_score():
-    # Récupération du titre du film à partir des paramètres de la requête
+def predict():
     title = request.args.get('title')
-    # Recherche des caractéristiques du film à partir de son titre
+    model_type = request.args.get('model')
+    
+    if model_type == 'linear_regression':
+        model_file = 'data/modele_linear_regression.pkl'
+    elif model_type == 'random_forest':
+        model_file = 'data/modele_random_forest.pkl'
+    else:
+        return jsonify({'error': 'Modèle non valide'}), 400
+
+    global model
+    model = joblib.load(model_file)
+
     film_data = data[data['movie_title'] == title]
     if film_data.empty:
         return jsonify({'error': 'Film non trouvé'}), 404
-    # Extraction des caractéristiques du film
+
     features = film_data.drop(['imdb_score', 'movie_title'], axis=1).values
-    # Prédiction
     score_predict = model.predict(features)
-    # Récupération du score IMDB
     score_source = film_data['imdb_score'].values[0]
-    # Retour
-    return jsonify({'score_predict': score_predict[0], 'score_source': score_source})
+
+    if request.headers.get('Content-Type') == 'application/json':
+        return jsonify({'score_predict': score_predict[0], 'score_source': score_source})
+    else:
+        return render_template('result.html', score_predict=score_predict[0], score_source=score_source)
 
 
 if __name__ == '__main__':
